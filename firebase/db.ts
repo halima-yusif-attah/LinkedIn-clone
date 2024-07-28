af58bf15d449fa0acdf9d780f54c793c29033385
Halima Yusif-Attah <halima.yusif-attah@amalitech.org>
2024-07-28 05:43:14 +0000

    fix: modified the env file

--- firebase/db.ts
+++ firebase/db.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEYFIREBASE_API_KEY,
   authDomain: "linkedin-clone-2-bc3d7.firebaseapp.com",
   projectId: "linkedin-clone-2-bc3d7",
-  storageBucket: "linkedin-clone-2-bc3d7.appspot.com",
+  storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: "107967880776",
   appId: "1:107967880776:web:d9f9f7d4ac1798e8b8827d"
 };
