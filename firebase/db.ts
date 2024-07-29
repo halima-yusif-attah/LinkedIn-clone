8e5e393cfa2073ccdf4909150b7024b0d3b7c93d
Halima Yusif-Attah <halima.yusif-attah@amalitech.org>
2024-07-29 17:08:11 +0000

    fix: modified firebase database

--- firebase/db.ts
+++ firebase/db.ts
@@ -6,13 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  authDomain: "linkedin-clone-d8d15.firebaseapp.com",
+  projectId: "linkedin-clone-d8d15",
+  storageBucket: "linkedin-clone-d8d15.appspot.com",
+  messagingSenderId: "67554998688",
+  appId: "1:67554998688:web:c5114f434edafaaef48cc5"
 };
-
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
